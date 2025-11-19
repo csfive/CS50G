65efdecc54f68641ae64ee4a1b7db248f5436cc3
mancuoj <mancuoj@gmail.com>
2025-11-20 00:49:26 +0800

    add sounds

--- week0-pong/main.lua
+++ week0-pong/main.lua
@@ -18,6 +18,12 @@ function love.load()
     scoreFont = love.graphics.newFont('font.ttf', 32)
     love.graphics.setFont(smallFont)
 
+    sounds = {
+        ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static')
+    }
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -56,6 +62,8 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 150)
             end
+
+            sounds['paddle_hit']:play()
         end
 
         if ball:collides(player2) then
@@ -68,22 +76,27 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 150)
             end
+
+            sounds['paddle_hit']:play()
         end
 
         if ball.y <= 0 then
             ball.y = 0
             ball.dy = -ball.dy
+            sounds['wall_hit']:play()
         end
 
         if ball.y >= VIRTUAL_HEIGHT - 4 then
             ball.y = VIRTUAL_HEIGHT - 4
             ball.dy = -ball.dy
+            sounds['wall_hit']:play()
         end
 
         if ball.x < 0 then
             -- 左边出界，右边 2 号得分，得分后对面 1 号发球
             servingPlayer = 1
             player2Score = player2Score + 1
+            sounds['score']:play()
 
             if player2Score == WINNING_SCORE then
                 winningPlayer = 2
@@ -97,6 +110,7 @@ function love.update(dt)
         if ball.x > VIRTUAL_WIDTH then
             servingPlayer = 2
             player1Score = player1Score + 1
+            sounds['score']:play()
 
             if player1Score == WINNING_SCORE then
                 winningPlayer = 1
