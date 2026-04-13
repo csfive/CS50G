d04deab058a603d5fb72e2cb730e5507b07b6b39
mancuoj <mancuoj@gmail.com>
2026-04-14 02:36:30 +0800

    ref

--- 0-pong/main.lua
+++ 0-pong/main.lua
@@ -1,5 +1,30 @@
-function love.load()
+push = require 'push'
 
+-- actual window size
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+-- emulated window size by push
+VIRTUAL_WIDTH, VIRTUAL_HEIGHT = 432, 243
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.window.setTitle("Pong")
+    math.randomseed(os.time())
+
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    largeFont = love.graphics.newFont('font.ttf', 16)
+    scoreFont = love.graphics.newFont('font.ttf', 32)
+    love.graphics.setFont(smallFont)
+
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        resizable = false,
+        fullscreen = false
+    })
+    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'normal' })
+end
+
+function love.resize(w, h)
+    push.resize(w, h)
 end
 
 function love.update(dt)
@@ -7,5 +32,14 @@ function love.update(dt)
 end
 
 function love.draw()
+    push.start()
+    displayFPS()
+    push.finish()
+end
 
+function displayFPS()
+    love.graphics.setFont(smallFont)
+    love.graphics.setColor(0, 255 / 255, 0, 255 / 255)
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
+    love.graphics.setColor(255, 255, 255, 255)
 end
