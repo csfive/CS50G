aed87319997027295da67afce05a411acca945d3
mancuoj <mancuoj@gmail.com>
2026-05-07 21:06:36 +0800

    init

--- 1-flappy-bird/main.lua
+++ 1-flappy-bird/main.lua
@@ -1,11 +1,42 @@
-function love.load()
+push = require 'push'
 
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+VIRTUAL_WIDTH, VIRTUAL_HEIGHT = 512, 288
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.window.setTitle('Flappy Bird')
+
+    background = love.graphics.newImage('background.png')
+    ground = love.graphics.newImage('ground.png')
+
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
+
+    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'normal' })
+end
+
+function love.resize(w, h)
+    push.resize(w, h)
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
 end
 
 function love.update(dt)
-
 end
 
 function love.draw()
+    push.start()
 
+    love.graphics.draw(background, 0, 0)
+    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+
+    push.finish()
 end
