3c225cc8406a6af467e11b175c7b297962fc089d
mancuoj <mancuoj@gmail.com>
2026-05-12 00:14:01 +0800

    sounds

--- 1-flappy-bird/main.lua
+++ 1-flappy-bird/main.lua
@@ -40,11 +40,11 @@ function love.load()
     }
 
     gSounds = {
-        ['jump'] = love.audio.newSource('jump.wav', 'static'),
-        ['explosion'] = love.audio.newSource('explosion.wav', 'static'),
-        ['hurt'] = love.audio.newSource('hurt.wav', 'static'),
-        ['score'] = love.audio.newSource('score.wav', 'static'),
-        ['music'] = love.audio.newSource('marios_way.mp3', 'static')
+        ['jump'] = love.audio.newSource('sounds/jump.wav', 'static'),
+        ['explosion'] = love.audio.newSource('sounds/explosion.wav', 'static'),
+        ['hurt'] = love.audio.newSource('sounds/hurt.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['music'] = love.audio.newSource('sounds/marios_way.mp3', 'static')
     }
 
     gSounds['music']:setLooping(true)
